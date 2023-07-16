facf19276dfcdd0fb3f75fcece8ff72b6a435e52
Davi Marcilio <davimarcilio.js@gmail.com>
2023-07-16 00:13:33 -0300

    test: start authenticator
    Co-authored-by: polaguilherme <polaguilherme@users.noreply.github.com>

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import { prisma } from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth, { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: AuthOptions = {
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_SECRET_ID!,
+    }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
