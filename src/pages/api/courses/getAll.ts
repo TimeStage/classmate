49c78e811b99ec80a95f05842710d24d39b57d9c
polaguilherme <guilhermepola03@gmail.com>
2023-07-23 14:31:17 -0300

    fix pending issues

--- src/pages/api/courses/getAll.ts
+++ src/pages/api/courses/getAll.ts
@@ -4,21 +4,26 @@ import { prisma } from "@/lib/prisma";
 
 
 
-export default async function handler(
+export default async function GetAll(
     req: NextApiRequest, 
     res: NextApiResponse
 
     ) {
+     
 
-    if (req.method === 'GET') {
         try {
+
+            if (req.method !== "GET") {
+                return res.status(405)
+            }
+
             const course = await prisma.course.findMany()
             res.status(200).json(course)
         } catch (error) {
             console.error(error);
-            res.status(500).json({error: "Erro ao buscar cursos"})
+            res.status(500).json({error: "error when searching for courses"})
             
         }
-    }
+
     
 }
